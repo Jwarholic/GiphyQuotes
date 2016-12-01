d4756b704eb9ce7cc93d9eb2a0dfb1265dcc2f55
Jason Warholic <warholic.jason@gmail.com>
2016-12-01 13:01:45 -0800

    Add quote api logic module

---
+++ app/models/quote.rb
@@ -0,0 +1,65 @@
+require 'pry'
+require 'net/http'
+require 'uri'
+require 'json'
+
+
+module Quote
+	KEY = ENV['QUOTES']
+
+def self.quote(word)
+
+	uri = URI.parse("http://quotes.rest/quote.json?category=#{word}")
+
+	request = Net::HTTP::Get.new(uri)
+	request["X-Theysaidso-Api-Secret"] = KEY
+
+	response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+	  http.request(request)
+
+	 conversion = JSON.parse(response.body)
+	 #returns the quote
+	 conversion["contents"]["quotes"][0]["quote"]
+	end
+
+
+
+end
+
+
+end
+
+
+
+
+
+# uri = URI.parse("http://quotes.rest/quote/categories.json?start=10000")
+
+
+# uri = URI.parse("http://quotes.rest/qod.json?category=management")
+# request = Net::HTTP::Get.new(uri)
+# request["X-Theysaidso-Api-Secret"] = "F5EKHhzShg1HED9cvzyvTAeF"
+
+# response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+#   http.request(request)
+# end
+
+#IF A QUOTE IS FOUND THIS IS THE WAY TO GRAB THE TEXT.
+# conversion = JSON.parse(response.body)
+# puts conversion["contents"]["quotes"][0]["quote"]
+#ELSE 
+#SEND NO CATEGORY FOUND TO USER
+#send back no results found to the user.
+#conversion["contents"]["quotes"].nil?
+
+
+#10 RESPONSES
+#academics
+#adaptation
+#afraid
+#alive
+#cautious
+#celebrate
+#embarrassing
+#emotional	
+
